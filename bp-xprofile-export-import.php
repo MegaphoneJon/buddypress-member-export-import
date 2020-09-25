cb8f024b8fdc1fe8619ba0c2724f0787d5b7e0eb
wbcomdev <pallavi@wbcomdesigns.com>
2020-09-25 18:14:27 +0530

    Revert "Added wbcom wrapper"
    This reverts commit 432f7d7858f6502ea49af02169ec3906b3473c2a.

--- bp-xprofile-export-import.php
+++ bp-xprofile-export-import.php
@@ -33,13 +33,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
 * Constants used in the plugin.
 */
-if ( ! defined( 'BPXP_PLUGIN_PATH' ) ) {
-	define( 'BPXP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
-}
-if ( ! defined( 'BPXP_PLUGIN_URL' ) ) {
-	define( 'BPXP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
-
-}
+define( 'BPXP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+define( 'BPXP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 
 if ( ! function_exists( 'bpxp_admin_page_link' ) ) {
 
